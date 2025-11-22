4882ffc864543b5494d5a74e220a8e9c7eef54c9
1eehvunzin <hyunjinlee@ewha.ac.kr>
2025-11-23 08:04:50 +0900

    fix : -

--- src/components/Histogram.jsx
+++ src/components/Histogram.jsx
@@ -44,19 +44,30 @@ export default function Histogram({ courseId }) {
 
   return (
     <div style={{ display: "flex", gap: "8px", alignItems: "flex-end" }}>
-      {histData.map((b, i) => (
-        <div key={i} style={{ display: "flex", flexDirection: "column" }}>
-          <div
-            style={{
-              backgroundColor: "#007aff",
-              width: "30px",
-              height: `${b.value * 4}px`,
-            }}
-            title={b.range}
-          />
-          <h4 style={{ color: "#444548" }}>{b.range}</h4>
-        </div>
-      ))}
+      {histData.map((b, i) => {
+        // my_cumulative_score가 속한 구간인지 확인
+        let isMyScoreRange = false;
+        if (myCumulativeScore !== null) {
+          const [min, max] = b.range.split("-").map(Number);
+          isMyScoreRange = myCumulativeScore >= min && myCumulativeScore < max;
+        }
+
+        return (
+          <div key={i} style={{ display: "flex", flexDirection: "column" }}>
+            <div
+              style={{
+                backgroundColor: isMyScoreRange ? "#FF8C00" : "#007aff",
+                width: "30px",
+                height: `${b.value * 4}px`,
+              }}
+              title={b.range}
+            />
+            <h4 style={{ color: isMyScoreRange ? "#FF8C00" : "#d9d9d9" }}>
+              {b.range}
+            </h4>
+          </div>
+        );
+      })}
     </div>
   );
 }
